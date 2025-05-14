a796142b7b9013f035c923fc062502a5ddbbd757
Arthur <arthur.rob.dev@gmail.com>
2025-05-14 18:11:47 +0200

    core(dep): add vue-router and vuetify, initialize structure

---
+++ src/views/CreateAccount.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="create-account-wrapper">
+        <h2>Create Account</h2>
+        <p>Already have an account ? Go to <router-link to="/">Login</router-link></p>
+    </div>
+</template>
---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="login-wrapper">
+        <h2>Login</h2>
+        <v-form @submit.prevent="handleLogin">
+            <v-text-field
+                v-model="email"
+                label="Email"
+                required
+            ></v-text-field>
+            <v-text-field
+                v-model="password"
+                label="Password"
+                type="password"
+                required
+            ></v-text-field>
+            <v-btn @click="handleLogin">Login</v-btn>
+        </v-form>
+        <p>Don't have an account? <router-link to="/create-account">Create one</router-link></p>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+
+const handleLogin = () => {
+    // Handle login logic here
+    console.log('Logging in with', email.value, password.value);
+};
+</script>
